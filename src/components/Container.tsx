2b28a6b96a2d639404d8ec7939327820f4566c08
eliasfrigard <54836612+eliasfrigard@users.noreply.github.com>
2024-07-20 11:16:14 +0300

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -9,11 +9,11 @@ const Container = ({
   className?: string
 }) => {
   return (
-    <AnimateIn className='w-full flex justify-center items-center px-3 md:px-0'>
+    <div className='w-full flex justify-center items-center px-3 md:px-0'>
       <div className={`w-full h-full container ${className}`}>
         {children}
       </div>
-    </AnimateIn>
+    </div>
   )
 }
 
