552743f2c8f497757183f78c4ee8ee426ab5a91d
Elias Frigård <elias.frigard@tvtools.fi>
2024-08-04 19:05:21 +0300

    update animatein

--- src/components/AnimateIn.tsx
+++ src/components/AnimateIn.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer'
 const AnimateIn = ({
   children,
   className = '',
-  threshold = 0.2,
+  threshold = 0.1,
   triggerOnce = true,
   animationType = 'fade',
   slideDirection = 'left',
